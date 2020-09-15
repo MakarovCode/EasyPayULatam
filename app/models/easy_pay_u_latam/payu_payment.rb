95da7b5b4ab6b3db88252a6bb79fda5e5826459c
Simon C <simoncorreaocampo@gmail.com>
2020-09-15 12:08:13 -0500

    Changes

--- app/models/easy_pay_u_latam/payu_payment.rb
+++ app/models/easy_pay_u_latam/payu_payment.rb
@@ -24,29 +24,42 @@ module EasyPayULatam
       client = RApi::Client.new current_user.pay_u_costumer_id
       addcharge = RApi::AdditionalCharge.new client, self.reference_recurring_payment
       addcharge.params = {
-        "description": params[:description],
-        "additionalValues": {
+        "description" => params[:description],
+        "additionalValues" => {
           {
-            "name": "ITEM_VALUE",
-            "value": params[:value],
-            "currency": "COP"
+            "name" => "ITEM_VALUE",
+            "value" => params[:value],
+            "currency" => "COP"
           },
           {
-            "name": "ITEM_TAX",
-            "value": "0",
-            "currency": "COP"
+            "name" => "ITEM_TAX",
+            "value" => "0",
+            "currency" => "COP"
           },
           {
-            "name": "ITEM_TAX_RETURN_BASE",
-            "value": "0",
-            "currency": "COP"
+            "name" => "ITEM_TAX_RETURN_BASE",
+            "value" => "0",
+            "currency" => "COP"
           }
         }
       }
 
       addcharge.create!
 
+      unless addcharge.response["id"].blank?
+        self.additional_charges_data = "#{self.additional_charges_data}|#{addcharge.response["id"]}·#{Date.today}"
+      end
+
       addcharge
+    end
+
+    def get_additional_charges
+      charges = []
+      self.additional_charges_data.split("|").each do |charge|
+        data = charge.split("·")
+        charges.push({id: data[0], date: data[1]})
+      end
+      charges
     end
 
     def remove_charge(id)
